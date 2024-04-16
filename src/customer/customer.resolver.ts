4cdbb467fb63520120449aed4034d1cb71d01a42
HNP\sslmd01 <slopezmartin@sescam.jccm.es>
2024-04-16 10:24:17 +0200

    Create Project
    Create Project. Create InvoiceModel and CustomerModel ObjectTypes and CustomerResolver

---
+++ src/customer/customer.resolver.ts
@@ -0,0 +1,27 @@
+import { CustomerModel } from "src/invoice/customer.model";
+import { Resolver, Mutation, Args, Query, ResolveField, Parent} from '@nestjs/graphql'
+import { Inject } from "@nestjs/common";
+import { InvoiceModel } from "src/invoice/invoice.model";
+
+@Resolver(of => CustomerModel)
+export class CustomerResolver {
+    constructor(
+        @Inject(CustomerService) private customerService: CustomerService,
+        @Inject(InvoiceService) private invoiceService: InvoiceService
+    ) { }
+    @Query(returns => CustomerModel) 
+    async customer(@Args('id') id: string): Promise<CustomerModel> {
+        return await this.customerService.findOne(id)
+    }
+
+    @ResolveField(returns => [InvoiceModel])
+    async invoices(@Parent() customer): Promise<InvoiceModel[]> {
+        const { id } = customer;
+        return this.invoiceService.findByCustomer(id);
+    }
+
+    @Query(returns => [CustomerModel])
+    async customers(): Promise<CustomerModel[]> {
+        return await this.customerService.findAll()
+    }
+}
