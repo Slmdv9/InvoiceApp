ea59dba38f9e6bd60f637a706e9b2eabee391ea5
HNP\sslmd01 <slopezmartin@sescam.jccm.es>
2024-04-16 11:50:42 +0200

    Organise customer folder. Create CustomerDTO

--- src/customer/customer.resolver.ts
+++ src/customer/customer.resolver.ts
@@ -1,4 +1,4 @@
-import { CustomerModel } from "src/invoice/customer.model";
+import { CustomerModel } from "src/customer/customer.model";
 import { Resolver, Mutation, Args, Query, ResolveField, Parent} from '@nestjs/graphql'
 import { Inject } from "@nestjs/common";
 import { InvoiceModel } from "src/invoice/invoice.model";
--- src/customer/customer.service.ts
+++ src/customer/customer.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { CustomerDTO } from './customer.dto';
-import { CustomerModel } from 'src/invoice/customer.model';
+import { CustomerModel } from 'src/customer/customer.model';
 @Injectable()
 export class CustomerService {
     constructor(
