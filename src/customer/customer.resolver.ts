a2347dc10993f16fd37d015bf83527414c77b4e6
HNP\sslmd01 <slopezmartin@sescam.jccm.es>
2024-04-16 11:43:16 +0200

    Added createCustomer Mutation to CustomerResolver

--- src/customer/customer.resolver.ts
+++ src/customer/customer.resolver.ts
@@ -2,6 +2,7 @@ import { CustomerModel } from "src/invoice/customer.model";
 import { Resolver, Mutation, Args, Query, ResolveField, Parent} from '@nestjs/graphql'
 import { Inject } from "@nestjs/common";
 import { InvoiceModel } from "src/invoice/invoice.model";
+import { CustomerService } from "./customer.service";
 
 @Resolver(of => CustomerModel)
 export class CustomerResolver {
@@ -23,5 +24,15 @@ export class CustomerResolver {
     @Query(returns => [CustomerModel])
     async customers(): Promise<CustomerModel[]> {
         return await this.customerService.findAll()
+    }
+
+    @Mutation(returns => CustomerModel)
+    async createCustomer(
+        @Args('name') name: string,
+        @Args('email') email: string,
+        @Args('phone', {nullable: true }) phone: string,
+        @Args('address', { nullable: true }) address: string,        
+    ): Promise<CustomerModel> {
+        return await this.customerService.create({name, email, phone, address })
     }
 }
