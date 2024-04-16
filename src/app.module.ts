877a660d236db54b90678b0f4d326d282b0ac82f
HNP\sslmd01 <slopezmartin@sescam.jccm.es>
2024-04-16 12:54:15 +0200

    Configure DatabaseConfig in TypeOrmModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,21 +3,15 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { GraphQLModule } from '@nestjs/graphql';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import DatabaseConfig from './config/database.config';
+import { ConnectOptions } from 'typeorm';
 
 @Module({
   imports: [
     GraphQLModule.forRoot({
       autoSchemaFile: 'schema.gql'
     }),
-    TypeOrmModule.forRoot({
-      type: 'postgres',
-      host: 'localhost',
-      port: 5432,
-      password: '',
-      database: 'invoiceapp',
-      entities: ['dist/**/*.model.js'],
-      synchronize: false
-    })
+    TypeOrmModule.forRoot(DatabaseConfig() as ConnectOptions)
   ],
   controllers: [AppController],
   providers: [AppService],
